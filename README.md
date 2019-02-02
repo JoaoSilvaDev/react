4d43446794bccbf832e5e6fef12e0bc0d8df7a89
Joao Silva <joaosdev@gmail.com>
2019-02-02 19:57:48 +0000

    intialized repo, added realtime-search project

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# react
+# React Log #
+
+---
+
+_**1/2/19**_
+
+Last year I built a portfolio website with React for my girlfriend.
+I haven't touched React or webdev since then and I'm very very rust (coudln't even build a todo app)
+
+*3 months of heavy practicing and 6 months of not even looking at code*
+
+I'll be logging my practice here.
+
+**Disclaimers**:
+* This is not a tutorial.
+* I've worked through a third of React course on udemy but it wasn't working for me.
+* I have basic programming knowledge, I've done a lot of games using Unity (C#) and some webdev outside of React.
+
+---
+
+_**1/2/19**_
+
+Tried to build a todo app and failed
+
+Read this:
+* [How to Learn React - A roadmap from beginner to advance](https://medium.com/r/?url=https%3A%2F%2Fmedium.freecodecamp.org%2Flearning-react-roadmap-from-scratch-to-advanced-bff7735531b6)
+* [React Documentation](https://reactjs.org/docs/hello-world.html)
+* [All the fundamental React.js concepts, jammed into this single Medium article]()
+* [Official React Tutorial](https://reactjs.org/tutorial/tutorial.html)
+
+---
+
+_**2/2/19**_
+
+Built a realtime search bar that filters an array of strings
---
+++ realtime-search/src/App.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import './css/App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      items: [
+        "shark",
+        "whale",
+        "tiger",
+        "lion",
+        "giraffe",
+        "racoon",
+        "golfin",
+        "crocodile",
+        "pig",
+        "serpent",
+        "snake",
+        "moose",
+        "octopus",
+        "monkey",
+        "fox",
+        "wolf",
+        "lemur",
+        "kangaroo",
+        "duck",
+        "chicken",
+        "koala",
+        "panda",
+        "bear",
+      ],
+      searchInput: ''
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({searchInput: e.target.value});
+  }
+
+  GetList() {
+    const result = this.state.items.filter(i => i.includes(this.state.searchInput));
+    const listItems = result.map((d) => <li key={d} className="list-item">{d}</li>);
+    return <ul className="list">{listItems}</ul>;  
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <input type="text" value={this.state.searchInput} onChange={this.handleChange.bind(this)} className="search-bar"/>
+        {this.GetList()}
+      </div>
+    );
+  }
+}
+
+export default App;
